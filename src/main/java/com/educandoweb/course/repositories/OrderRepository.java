12c5fc9af52641de8c7919d7c8900b2ac61f167f
Guilherme Guida <gguilhermecontato1@gmail.com>
2024-10-28 11:21:27 -0300

    Criação do user update.

--- src/main/java/com/educandoweb/course/repositories/OrderRepository.java
+++ src/main/java/com/educandoweb/course/repositories/OrderRepository.java
@@ -4,6 +4,4 @@ import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
+public interface OrderRepository extends JpaRepository<Order, Long> {}
--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.educandoweb.course.resources;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
+import jakarta.persistence.PostUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -40,5 +41,11 @@ public class UserResource {
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         userService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        user = userService.update(id, user);
+        return ResponseEntity.ok().body(user);
     }
 }
